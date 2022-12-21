cdd6b7746d657c9c8fb4ea72d17c155bee079d46
abdelbari <abdomessah@gmail.com>
2022-12-21 21:39:56 +0100

    push

---
+++ day00/ex00/megaphone.cpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abdelbari <abdelbari@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/21 20:38:34 by abdelbari         #+#    #+#             */
+/*   Updated: 2022/12/21 21:36:35 by abdelbari        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	int i;
+	int j;
+
+	i = 1;
+	if(argc == 1)
+	{
+		std::cout << "*LOUD AND UNBEARABLE FEEDBACK NOISE*" << std::endl;
+	}
+	while(argv[i])
+	{
+		j = 0;
+		while(argv[i][j])
+		{
+			putchar(toupper(argv[i][j]));
+			j++;
+		}
+		std::cout << " ";
+		i++;
+	}
+	std::cout << std::endl;
+}
